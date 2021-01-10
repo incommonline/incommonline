b4a05c5eb1e71f97e65060498c001ded241ee362
Dominic Shillingford <dshillingford77@gmail.com>
2021-01-09 18:11:54 -0800

    Added a more sane skeleton for room endpoints

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
-import { Container, Row, Col } from 'react-bootstrap';
-import { BrowserRouter as Router, Switch, Route, Link, useParams } from 'react-router-dom';
+import { Container, Row, Col, Form } from 'react-bootstrap';
+import { BrowserRouter as Router, Switch, Route, useParams } from 'react-router-dom';
 
 import Home from "./Home"
+
+// stubs
+function getRoomName( roomId ) {
+  return getRoomDetails( roomId, "deadbeef" ).roomName;
+}
+
+function getRoomDetails( roomId, userId ) {
+  return {
+    currentUserId: userId,
+    roomId: roomId,
+    roomName: "Choral Reef",
+    niches: [ { nicheId: "TWwyAzRd", nicheName: "Hockey", isSupportedByUser: false, currentSupport: 3, supportRequired: 4 }, 
+              { nicheId: "biiTCxey", nicheName: "K-Pop",  isSupportedByUser: true,  currentSupport: 1, supportRequired: 4 } ],
+    subgroups: [ { subgroupId: "abcdefgh", subgroupName: "Vocal Jazz", memberIds: [ "1a2b3c4d", "2a3b4c5d" ] } ]
+  }
+}
+
 
 function Test() {
   let { roomId } = useParams();
@@ -10,7 +27,10 @@ function Test() {
     <Container>
       <Row>
         <Col>
-          <p>{roomId}</p> 
+          <h1>Welcome to {getRoomName( roomId )}. Who are you?</h1> 
+          <Form>
+            <Form.Control/>
+          </Form>
         </Col>
       </Row>
     </Container>
@@ -18,17 +38,6 @@ function Test() {
 }
 
 export default function App() {
-  let sampleFromApi = 
-    {
-      currentUserId: "abcdefgh",
-      roomId: "hDBshCvM",
-      roomName: "Choral Reef",
-      niches: [ { nicheId: "TWwyAzRd", nicheName: "Hockey", isSupportedByUser: false, currentSupport: 3, supportRequired: 4 }, 
-                { nicheId: "biiTCxey", nicheName: "K-Pop",  isSupportedByUser: true,  currentSupport: 1, supportRequired: 4 } ],
-      subgroups: [ { subgroupId: "abcdefgh", subgroupName: "Vocal Jazz", memberIds: [ "1a2b3c4d", "2a3b4c5d" ] } ]
-    }
-
-
   return (
     <Router>
       <Switch>
